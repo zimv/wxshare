afd43842e1cc1cf5b9fd02fb9347e608e18c53d0
mayiwen <mayiwen@fishsaying.com>
2015-05-25 10:45:25 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+#js微信封装
+
+##使用帮助
+
+###如果分享的内容都一致(all the same)
+	wxshare.link = '';
+    wxshare.desc = '';
+    wxshare.imgUrl = '';
+    wxshare.type = '';
+    wxshare.dataUrl = '';
+    wxshare.start();
+
+###如果有不一致,针对不一致的地方进行修改(some different)
+	
+	wxshare.link = '';
+    wxshare.title = '';
+    wxshare.desc = '';
+    wxshare.imgUrl = '';
+
+    wxshare.setData();//必须加上
+    wxshare.TimeLineData.title = ''; 
+    wxshare.QQData.desc = '';
+    wxshare.start();
+
+###重点说明: setData();
+
+如果全部的分享内容都一样，直接执行start(), 判断data属性空为true，会执行setData()，
+为四个分享赋值:QQData data TimeLineData WeiboData, 而后初始化
+
+如果有不一致，首次赋值后，直接调用setData(),为四个参数赋值，之后再修改不同的参数，最后start()初始化。
+        
---
+++ wxshare.js
@@ -0,0 +1,100 @@
+module.exports = {
+    url : 'your route' + encodeURIComponent(window.location.href),
+    link : '',  //分享链接
+    title : '',  //分享标题
+    desc : '',  //分享描述
+    imgUrl : '', //分享图标
+    type : '', //分享类型,music、video或link，不填默认为link
+    dataUrl : '', ////如果type是music或video，则要提供数据链接，默认为空
+
+    data: '',
+    QQData: '',
+    TimeLineData: '',
+    WeiBoData: '',
+
+    setData: function(){
+        var self = this;
+        self.data = {
+            title: self.title,
+            desc: self.desc,
+            link: self.link,
+            imgUrl: self.imgUrl,
+            type: self.type, // 分享类型,music、video或link，不填默认为link
+            dataUrl: self.dataUrl, // 如果type是music或video，则要提供数据链接，默认为空
+            success: function () {
+                //alert('用户确认分享后执行的回调函数');
+            },
+            cancel: function () {
+                //alert('用户取消分享后执行的回调函数');
+            }
+        };
+        //深度克隆
+        self.QQData = JSON.stringify(self.data);
+        self.QQData = JSON.parse(self.QQData);
+        self.TimeLineData = JSON.stringify(self.data);
+        self.TimeLineData = JSON.parse(self.TimeLineData);  
+        self.WeiBoData = JSON.stringify(self.data);
+        self.WeiBoData = JSON.parse(self.WeiBoData);       
+    },
+
+    start : function(){
+        var self = this;
+        $.get(self.url, function(datas){
+            var datas = datas.data;
+            wx.config({
+                debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                appId: 'wxe945cda35f24b4d7', // 必填，公众号的唯一标识
+                timestamp:datas.signs.timestamp , // 必填，生成签名的时间戳
+                nonceStr: datas.signs.nonceStr, // 必填，生成签名的随机串
+                signature: datas.signs.signature,// 必填，签名，见附录1
+                jsApiList: [
+                    'checkJsApi',
+                    'onMenuShareTimeline',
+                    'onMenuShareAppMessage',
+                    'onMenuShareQQ',
+                    'onMenuShareWeibo',
+                    'hideMenuItems',
+                    'showMenuItems',
+                    'hideAllNonBaseMenuItem',
+                    'showAllNonBaseMenuItem',
+                    'translateVoice',
+                    'startRecord',
+                    'stopRecord',
+                    'onRecordEnd',
+                    'playVoice',
+                    'pauseVoice',
+                    'stopVoice',
+                    'uploadVoice',
+                    'downloadVoice',
+                    'chooseImage',
+                    'previewImage',
+                    'uploadImage',
+                    'downloadImage',
+                    'getNetworkType',
+                    'openLocation',
+                    'getLocation',
+                    'hideOptionMenu',
+                    'showOptionMenu',
+                    'closeWindow',
+                    'scanQRCode',
+                    'chooseWXPay',
+                    'openProductSpecificView',
+                    'addCard',
+                    'chooseCard',
+                    'openCard'
+                ] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+            });
+
+            wx.ready(function(){
+                wx.showOptionMenu();
+                if(self.data == '') self.setData();
+                wx.onMenuShareQQ(self.QQData);
+                wx.onMenuShareAppMessage(self.data);
+                wx.onMenuShareTimeline(self.TimeLineData);
+                wx.onMenuShareWeibo(self.WeiBoData)
+
+            });
+        });
+
+    }
+}
